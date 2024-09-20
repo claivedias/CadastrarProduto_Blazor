61c6664b949a7110d5920a0114e82c5811f2b61b
Claive Dias <claivedias@outlook.com>
2024-09-20 14:19:18 -0300

    add
    add

---
+++ ProjetoInicial/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoInicial.Models
+{
+    public class Produto
+    {
+        [Required(ErrorMessage ="Campo obrigatório")]
+        public string Nome { get; set; }
+
+        [Range(0,99999999, ErrorMessage ="Informe um valor adequado")]
+        public int Quantidade { get; set; }
+
+        [Range(0.1,99999999, ErrorMessage ="Informe um preço adequado"), ]
+        public double Preco { get; set; }
+
+        public double ValorEstoqueTotal()
+        {
+            return Preco*Quantidade;
+        }
+    }
+}
